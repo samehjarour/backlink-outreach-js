9370d62d5ab6da73fed5c37c9fba6734f0c8b036
Daniil Poletaev <44584010+danpoletaev@users.noreply.github.com>
2025-02-20 18:30:21 +0100

    removed keys

--- src/ai-utils.ts
+++ src/ai-utils.ts
@@ -1,34 +1,23 @@
-import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { ChatOpenAI } from "@langchain/openai";
-import { ChatAnthropic } from "@langchain/anthropic";
 import { CRAFT_MESSAGE_SEQUENCE, FILTER_LINKS_PROMPT } from "./prompts.js";
-import { JsonOutputParser } from "@langchain/core/output_parsers";
 
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
-import { ChatOpenAI } from "@langchain/openai";
 
 import OpenAI from "openai";
 import { z } from "zod";
 import { zodResponseFormat } from "openai/helpers/zod";
 
+const { OPENAI_API_KEY } = process.env;
+
 const openai = new OpenAI({
-  apiKey:
-    "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",
+  apiKey: OPENAI_API_KEY,
 });
 
 const model = new ChatOpenAI({
   model: "gpt-4o-mini",
   temperature: 0,
-  apiKey:
-    "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",
-});
-
-const claude = new ChatAnthropic({
-  model: "claude-3-5-sonnet-20241022",
-  temperature: 0,
-  apiKey:
-    "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",
+  apiKey: OPENAI_API_KEY,
 });
 
 const Url = z.object({
